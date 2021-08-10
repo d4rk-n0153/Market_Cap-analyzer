0d4e2b715ff982fbfc9ac18264d0783cbfeff845
ali farvardin <ali_farvardin1992@protonmail.com>
2021-08-10 08:57:46 +0430

    update 1

--- market_cap_automate.py
+++ market_cap_automate.py
@@ -1,12 +1,10 @@
-﻿from bs4 import BeautifulSoup as bsp
-from json import loads as l
+﻿from json import loads as l
 import requests as rq
 import jdatetime as jd
 from pandas import *
-# from time import sleep
+
 url_ztitad=[]
 url_lastprice=[]
-# market_cap=[]
 total_shares=[]
 lastPrices=[]
 Symbols_counts=int(input('Please enter the number of your desired symbol: '))
@@ -15,27 +13,15 @@ sym=[input(f'pls type ur symbol {i}: ') for i in range (1,Symbols_counts+1)]
 gdate=str(jd.date(int(dat[0]),int(dat[1]),int(dat[2])).togregorian()).replace('-','')
 
 
-# #headers={
-#         #Accept: */*
-#         #Accept-Encoding: gzip, deflate
-#         #'Accept-Language':'en-US',
-#         #'Cache-Control': 'no-cache',
-#         #'Connection':'keep-alive',
-#         #'Cookie':'_ga=GA1.2.1843003826.1607405830; _gid=GA1.2.864430428.1624673785; __qca=P0-1856150858-1624680910808; ASP.NET_SessionId=be4aok1ffxxnmkyogchg2qmt; _gat_gtag_UA_63076930_1=1',
-#         #'Host': 'cdn.tsetmc.com',
-#         #'Pragma': 'no-cache',
-#         #Referer: http://cdn.tsetmc.com/Loader.aspx?ParTree=15131P&i=46348559193224090&d=20210621
-#         'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
-#     }
 url='https://d4rk-n0153.github.io/tsetmc/market-watch2.html'
-#url='http://tradersarena.ir/data/market-watch?mt=0&fi=none&_=1624808150318'
+
 
 response=rq.get(url)
 
 data=l(response.content.decode('utf-8-sig'))
 
 
-#symbols=[data[0].get(b) for i in range (0,len(data))]3
+
 
 nums=[data[0].get(b) for b in sym]
 dick=dict(zip(sym,nums))
@@ -58,7 +44,6 @@ for last_price in range(len(url_lastprice)):
     lastPrices.append(lastPrice)
 
 market_cap=[lastPrices[i]*total_shares[i]/(10000000000) for i in range(len(lastPrices))]
-print(market_cap)
                        
 df=DataFrame(index=(sym),columns=(sym))
 df.insert(len(market_cap),'ارزش بازار(میلیارد تومان)','none')
